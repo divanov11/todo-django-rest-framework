a8393f33041a27ecaba37be62c88eac5d72a8bbf
Monday Solomon <83866674+black-coda@users.noreply.github.com>
2023-01-10 23:15:11 +0100

    models updated

--- api/models.py
+++ api/models.py
@@ -4,7 +4,10 @@ from django.db import models
 
 class Task(models.Model):
   title = models.CharField(max_length=200)
+  body = models.TextField(null=True, blank=True)
   completed = models.BooleanField(default=False, blank=True, null=True)
+  created = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
       
   def __str__(self):
     return self.title
